07b0798a69f4b1ebc33117debd6c2fc44b306396
Nick - Shumnan Suengjitsiriroj <nick@webon.net>
2016-12-20 16:18:31 +0700

    Add installation bundle Improve example https://www.tutorialspoint.com/reactjs/reactjs_animations.htm

---
+++ npm_installation_bundle.bat
@@ -0,0 +1,15 @@
+::React installation bundle From step 3 and so on
+::https://www.tutorialspoint.com/reactjs/reactjs_environment_setup.htm
+call npm install webpack --save
+call npm install webpack-dev-server --save
+call npm install react --save
+call npm install react-dom --save
+call npm install react-addons-css-transition-group
+call npm install babel-core
+call npm install babel-loader
+call npm install babel-preset-react
+call npm install babel-preset-es2015
+copy NUL index.html
+copy NUL App.jsx
+copy NUL main.js
+copy NUL webpack.config.js
